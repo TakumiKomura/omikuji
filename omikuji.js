d098496375fe065daff29aa4d0e9ee7d92572fb8
TakumiKomura <takumi141219@outlook.jp>
2022-08-01 23:37:34 +0900

    [finish] homework

--- omikuji.js
+++ omikuji.js
@@ -12,15 +12,15 @@ if (rand == 0) {
     userresult = "大吉";
 }
 if (rand == 1) {
-userresult = "中吉";
+    userresult = "中吉";
 }
 if (rand == 2) {
-userresult = "小吉";
+    userresult = "小吉";
 }
 if (rand == 3) {
-userresult = "吉";
+    userresult = "吉";
 }
 if (rand == 4) {
-userresult = "凶";
+    userresult = "凶";
 }
 document.getElementById("result").innerHTML = userresult;
